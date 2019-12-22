55abed84a9f8a467298bfc5821c110b2c3a46d2f
a-pompom <aoi.matsuda.3x@gmail.com>
2019-12-22 16:58:07 +0900

    initial commit

---
+++ src/main/java/app/api/todo/controller/TodoController.java
@@ -0,0 +1,5 @@
+package app.api.todo.controller;
+
+public class TodoController {
+
+}
---
+++ src/main/java/app/api/todo/main/ApiTodoApplication.java
@@ -0,0 +1,13 @@
+package app.api.todo.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTodoApplication.class, args);
+	}
+
+}
